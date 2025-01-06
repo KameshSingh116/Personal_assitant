3ff5b1370e7bcaafa33888dae0281c75180925a7
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-06 22:46:04 +0530

    starting with speech_recognition

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 #Radhe Radhe 
 import speech_recognition as sr
-import pyaudio
-import  pyttsx3 , setuptools
+import pyaudio as pa
+import webbrowser as wb
+import  pyttsx3 
+import setuptools as st
+
+
+recognixer=sr.Recognizer() #Obatain the speech rrecognition functionality ability
+engine=pyttsx3.init() #To initialize the pyttsx3 module
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+if __name__=="__main__":
+    speak("Hey! Nice to meet you sir")
+    speak("How was you day !")
+
 
