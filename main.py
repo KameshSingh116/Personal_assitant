b49d708d35e281306c83295018892634577b9732
Kamesh Singh <radhakrishnapg152627@gmail.com>
2025-01-06 23:41:30 +0530

    Listeninig audio

--- main.py
+++ main.py
@@ -6,14 +6,29 @@ import  pyttsx3
 import setuptools as st
 
 
+
 recognixer=sr.Recognizer() #Obatain the speech rrecognition functionality ability
 engine=pyttsx3.init() #To initialize the pyttsx3 module
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
+
 if __name__=="__main__":
-    speak("Hey! Nice to meet you sir")
-    speak("How was you day !")
+    speak("Initializing Jarvis")
+    while True:
+     #Listen for the wake word "Mickey"
+    #Obtain audio from microphone
 
+     r=sr.Recognizer()
+     with sr.Microphone() as source:
+         print("Listening...")
+         audio=r.listen(source)
+         
+         
+         command=r.recognize_sphinx(audio)
+         print(command)
 
+    #
+     
